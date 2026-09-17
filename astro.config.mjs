7bb69b8fad84f6e25a5a67372ec9ea4efddf2395
Alexandre Oliveira <thexeco@gmail.com>
2026-09-17 17:30:42 -0300

    Avisando ao Google que o site existe av 2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://projeto-caneta-eight.vercel.app',
+  integrations: [
+    sitemap({
+      filter: (page) =>
+        !page.includes('nova-pagina') &&
+        !page.includes('pagina2') &&
+        !page.includes('pagina3'),
+    }),
+  ],
   build: {
     inlineStylesheets: 'always',
   },
